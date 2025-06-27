222283f705223742c4c6ca860f4080757d8eb0f6
rohan <rohanvagre2640@gmail.com>
2025-06-27 15:42:04 +0530

    eleven commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,43 @@ import Footer1 from './components/Footer/Footer1';
 import AdmissionsPage from './pages/Admissions';
 
 import ChatbotComponent from './components/Chatbot/ChatbotComponents';
+import DeveloperInfoPopup from './components/Devloperinfo/DeveloperinfoPopup';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admissions" element={<AdmissionsPage />} />
-      </Routes>
 
-      <Footer1/>
-    </Router>
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return (
+    <>
+      <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Rohan Sanjay Vagre"
+          studentPhotoUrl="/images/saniya.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admissions" element={<AdmissionsPage />} />
+        </Routes>
+
+        <Footer1 />
+      </Router>
+    </>
+
+
   );
 }
 
